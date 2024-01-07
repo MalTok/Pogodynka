a67358274c3973fa8da4f501ba3bcf0dcd35e162
MalTok <mig01@wp.pl>
2024-01-07 20:07:53 +0100

    Utworzenie zadania

---
+++ src/main/java/pl/mt/weatherapp/Zadanie311Application.java
@@ -0,0 +1,13 @@
+package pl.mt.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie311Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie311Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mt/weatherapp/Zadanie311ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mt.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie311ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
